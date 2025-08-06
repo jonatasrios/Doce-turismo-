8934ce2d0777d0c1f34ceaa40ccc2ef2edffeea2
Jonatas Rios <jonatasguimaraesg@gmail.com>
2025-08-06 19:06:01 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let ress = document.getElementById("ress")
 let resa = document.getElementById("resa")
+let reso = document.getElementById("reso")
+let resn = document.getElementById("resn")
 function agosto(){
     resa.innerHTML = `
                         <button onclick="voltar()">Voltar</button>
@@ -33,10 +35,136 @@ function agosto(){
 function setembro(){
     ress.innerHTML = `
                     <button onclick="voltar()">Voltar</button>
+                    <h3>Setembro</h3>
+                    <p>
+                        05 a 07 – Arraial do Cabo <br>
+                        05 a 07 – Cabo Frio <br>
+                        06 a 11 – Porto de Galinhas – <strong>ESGOTADO</strong><br>
+                        06 a 11 – Porto Seguro <br>
+                        06 a 10 – Caldas Novas <br>
+                        07 a 14 – Porto Seguro <br>
+                        09 a 16 – Porto de Galinhas <br>
+                        10 a 14 – Cabo Frio <br>
+                        11 a 14 – Expoflora <br>
+                        12 a 14 – São Tomé das Letras – <strong>ESGOTADO</strong><br>
+                        13 a 18 – Caldas Novas <br>
+                        14 – Festival Café com Biscoito <br>
+                        14 a 21 – Porto Seguro <br>
+                        16 a 23 – Peru – <strong>ESGOTADO</strong><br>
+                        18 a 21 – Angra dos Reis – <strong>ESGOTADO</strong><br>
+                        19 a 21 – Campos do Jordão <br>
+                        19 a 21 – Vesperata Diamantina <br>
+                        20 a 25 – Caldas Novas <br>
+                        21 a 28 – Porto Seguro <br>
+                        28/9 a 05/10 – Porto Seguro <br>
+                    </p>
+
+                    </p> `
+}
+function outubro(){
+    reso.innerHTML = `
+                    <button onclick="voltar()">Voltar</button>
+                    <h3>Outubro</h3>
+                    <p>
+                        03 a 05 – Cabo Frio <br>
+                        04 a 11 – Porto Seguro <br>
+                        05 a 12 – Porto Seguro <br>
+                        10 a 14 – Arraial do Cabo <br>
+                        10 a 16 – Porto Seguro <br>
+                        10 a 16 – Porto de Galinhas <br>
+                        11 a 15 – Olímpia – Thermas dos Laranjais <br>
+                        11 a 16 – Rio Quente / Caldas Novas <br>
+                        11 a 17 – Porto de Galinhas <br>
+                        12 a 16 – Porto Seguro <br>
+                        12 a 19 – Porto Seguro <br>
+                        12 a 19 – Oktoberfest <br>
+                        13 a 18 – Porto Seguro <br>
+                        15 a 19 – Olímpia – Thermas dos Laranjais <br>
+                        15 a 19 – Cabo Frio <br>
+                        15 a 19 – Caldas Novas <br>
+                        15 a 19 – Arraial do Cabo <br>
+                        17 a 19 – Campos do Jordão <br>
+                        17 a 19 – Cabo Frio <br>
+                        18 a 22 – Caldas Novas <br>
+                        19 a 23 – Porto Seguro <br>
+                        19 a 25 – Porto de Galinhas <br>
+                        19 a 26 – Porto Seguro <br>
+                        24 a 26 – Aparecida do Norte <br>
+                        24 a 26 – Cabo Frio <br>
+                        25 a 30 – Caldas Novas <br>
+                        26 a 30 – Porto Seguro <br>
+                        26 a 02/11 – Porto Seguro <br>
+                        30 a 06/11 – Gramado – Natal Luz <br>
+                        31 a 02/11 – Arraial do Cabo <br>
+    `
+}
+function novembro(){
+    resn.innerHTML = `<button onclick="voltar()">Voltar</button>
+            <h3>Novembro</h3>
+            <p>
+                01 a 06 – Porto Seguro <br>
+                01 a 06 – Porto de Galinhas – <strong>ESGOTADO</strong><br>
+                02 a 09 – Porto Seguro <br>
+                08 a 13 – Caldas Novas <br>
+                08 a 13 – Porto de Galinhas <br>
+                08 a 13 – Foz do Iguaçu – Três Fronteiras <br>
+                09 a 16 – Porto Seguro <br>
+                12 a 16 – Cabo Frio <br>
+                15 a 19 – Caldas Novas <br>
+                15 a 20 – Porto Seguro <br>
+                15 a 21 – Porto de Galinhas <br>
+                16 a 23 – Porto Seguro <br>
+                19 a 23 – Arraial do Cabo <br>
+                19 a 23 – Cabo Frio<br>
+                21 a 23 – Campos do Jordão<br>
+                21 a 23 – Cabo Frio<br>
+                23 a 30 – Porto Seguro<br>
+                28 a 30 – Aparecida do Norte<br>
+                28 a 30 – Cabo Frio<br>
+                30/11 a 04/12 – Porto Seguro<br>
+            </p>
 
     `
+}
+function dezembro(){
+    resd.innerHTML = `
+
+        <button onclick="voltar()">Voltar</button>
+        <h3>Dezembro</h3>
+        <p>
+            30/10 a 07/11 – Porto Seguro <br>
+            04 a 07 – Campos do Jordão <br>
+            05 a 07 – Cabo Frio <br>
+            06 a 10 – Olímpia – Thermas dos Laranjais <br>
+            06 a 11 – Caldas Novas <br>
+            07 a 10 – Cruzeiro Costa Favolosa <br>
+            07 a 14 – Porto Seguro <br>
+            10 a 14 – Cabo Frio <br>
+            12 a 14 – Cabo Frio <br>
+            12 a 14 – Campos do Jordão <br>
+            12 a 14 – Monte Verde <br>
+            12 a 14 – Arraial do Cabo <br>
+            13 a 16 – Caldas Novas <br>
+            14 a 21 – Porto Seguro <br>
+            19 a 21 – Cabo Frio <br>
+            21 a 26 – Caldas Novas – Natal <br>
+            21 a 28 – Porto Seguro – Natal <br>
+            26 a 28 – Cabo Frio <br>
+            27/12 a 02/01 – Porto Seguro – <strong>Réveillon</strong> <br>
+            28/12 a 02/01 – Caldas Novas – <strong>Réveillon</strong> <br>
+            28/12 a 03/01 – Macaé – <strong>Réveillon</strong> <br>
+            29/12 a 01/01 – Macaé – <strong>Réveillon</strong> <br>
+            30/12 a 04/01 – Arraial do Cabo – <strong>Réveillon</strong> <br>
+        </p>
+
+
+
+    `   
 }
 function voltar(){
     resa.innerHTML = ""
     ress.innerHTML = ""
+    reso.innerHTML = ""
+    resn.innerHTML = ""
+    resd.innerHTML = ""
 }
